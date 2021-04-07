61e59adc7cf884fb5cc292f39c7bebfd706f2c22
gaycodegal <sarahmavenoro@gmail.com>
2021-04-06 22:54:20 -0400

    initial commit

---
+++ test/comic-runner.js
@@ -0,0 +1,6 @@
+
+window.addEventListener("load", function() {
+    new Speech(10, 10, 200, 50, "hello!");
+    new Speech(300, 10, 200, 40, "hi!");
+    new Speech(10, 200, 200, 40, "???");
+});
---
+++ test/speech-bubble.js
@@ -0,0 +1,60 @@
+
+class Speech {
+    constructor(x, y, w, h, text) {
+	const jel = $(`
+<div class="speech">
+<div class="speech-inner" tabindex="0">
+<div class="speech-controls">
+<button class="prev">&lt;</button>
+<button class="next">&gt;</button>
+</div>
+<div class="speech-text"></div>
+</div></div>`);
+	const speechInner = jel.find(".speech-inner");
+	speechInner.find(".speech-text").text(text);
+	speechInner.find(".next")[0].onclick = (event)=>focusOnNext(event.target);
+	speechInner.find(".prev")[0].onclick = (event)=>focusOnPrevious(event.target);
+	jel.css("margin-left", x);
+	jel.css("margin-top", y);
+	jel.css("width", w);
+	jel.css("height", h);
+	speechInner.css("background", "red");
+	$("#world").append(jel);
+    }
+}
+
+function currentSpeech(currentControl) {
+    const current = currentControl.closest(".speech-inner");
+    const tabs = document.querySelectorAll(".world .speech-inner");
+    let currentIndex = -1;
+    for (var i = 0; i < tabs.length; ++i) {
+	if (tabs[i] == current) {
+	    currentIndex = i;
+	    break;
+	}
+    }
+
+    return {currentIndex, tabs};
+}
+
+function focusOnNext(currentControl) {
+    const result = currentSpeech(currentControl);
+    const currentIndex = result.currentIndex;
+    const tabs = result.tabs;
+    
+    if (currentIndex != -1) {
+	const next = (currentIndex + 1) % tabs.length;
+	tabs[next].focus();
+    }
+}
+
+function focusOnPrevious(currentControl) {
+    const result = currentSpeech(currentControl);
+    const currentIndex = result.currentIndex;
+    const tabs = result.tabs;
+    
+    if (currentIndex != -1) {
+	const next = ((currentIndex - 1) % tabs.length + tabs.length) % tabs.length;
+	tabs[next].focus();
+    }
+}
